1508dbd1d7fa14da586ff52e9d3db71417a54ad0
derektliu <liu.derekt@gmail.com>
2016-08-01 15:04:31 -0700

    Completed Base Requirements for Dancers refactoring

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -5,7 +5,7 @@ describe('blinkyDancer', function() {
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    blinkyDancer = new BlinkyDancer(10, 20, timeBetweenSteps);
   });
 
   it('should have a jQuery $node object', function() {
@@ -22,9 +22,14 @@ describe('blinkyDancer', function() {
     it('should call step at least once per second', function() {
       sinon.spy(blinkyDancer, 'step');
       expect(blinkyDancer.step.callCount).to.be.equal(0);
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+      blinkyDancer = new BlinkyDancer(10, 20, timeBetweenSteps);
+      sinon.spy(blinkyDancer, 'step');
+
+      // console.log(blinkyDancer.step.callCount);
+      clock.tick(timeBetweenSteps);
       clock.tick(timeBetweenSteps);
 
+      // console.log(blinkyDancer.step.callCount);
       expect(blinkyDancer.step.callCount).to.be.equal(1);
 
       clock.tick(timeBetweenSteps);
